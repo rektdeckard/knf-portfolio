ed0f376dab994c2aac23612d3eae126e199c6356
rektdeckard <friedtm@gmail.com>
2021-02-03 19:11:57 -0500

    Content: be more style-y

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,22 +1,14 @@
-import React from "react";
 import styled from "styled-components";
 
-const ContentWrapper = styled.div`
+const Content = styled.div`
   display: grid;
   place-items: center;
-  color: ${(props) => props.color};
+  color: ${({ color = "#DD8888"}) => color};
   width: 100%;
 
   & a {
     color: inherit;
   }
 `;
-
-const Content: React.FC<{ color?: string }> = ({
-  color = "#DD8888",
-  children,
-}) => {
-  return <ContentWrapper color={color}>{children}</ContentWrapper>;
-};
 
 export default Content;
